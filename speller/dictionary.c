81f57669de57a798c499a716e57cdb05aa378170
Tamirlan Sulaimanov <95983185+itimi01@users.noreply.github.com>
2022-08-12 19:22:16 +0300

    Update dictionary.c

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,7 +1,11 @@
 // Implements a dictionary's functionality
 
-#include <ctype.h>
-#include <stdbool.h>
+#include <ctype.h>       // toupper
+#include <stdbool.h>     // bool
+#include <stdlib.h>      // malloc; free
+#include <stdio.h>       // printf; fopen; fclose; fscanf
+#include <string.h>      // strcpy
+#include <strings.h>     // strcasecmp
 
 #include "dictionary.h"
 
@@ -13,8 +17,11 @@ typedef struct node
 }
 node;
 
+// Word count
+unsigned int word_count = 0;
+
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 150000;
 
 // Hash table
 node *table[N];
@@ -22,7 +29,19 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // индексирует в хэш слово и выдает тот же хэш
+    int hash_index  = hash(word);
+    node *cursor = table[hash_index];
+
+    while (cursor != NULL)
+    {
+        // Compare words
+        if (strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
@@ -30,26 +49,84 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    unsigned long sum = 0;
+    for (int j = 0; j < strlen(word); j++)
+    {
+        sum += tolower(word[j]);
+    }
+    return sum % N ;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // Open dictionary file
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    // Declare variable
+    char buffer_word[LENGTH + 1];
+
+    // Read string from file one at a time
+    while (fscanf(file, "%s", buffer_word) != EOF)
+    {
+        // Create a new node for each word
+        node *cursor = malloc(sizeof(node));
+        if (cursor == NULL)
+        {
+            return false;
+        }
+
+        // Copy word into node
+        strcpy(cursor->word, buffer_word);
+
+        // Hash word to obtain a hash value
+        int hash_index = hash(buffer_word);
+
+        // Insert node into hash table at that location
+        cursor->next = table[hash_index]; // cursor установили на текущий head
+        table[hash_index] = cursor; // a head на новый список cursor
+
+        // считаем количество word
+        word_count++;
+    }
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
+    if (word_count > 0)
+    {
+        return word_count;
+    }
+
     return 0;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+
+        while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+        if (table[i] == NULL)
+        {
+            return true;
+        }
+    }
+
     return false;
 }
